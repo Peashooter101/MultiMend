dfd01fb706da7ef71b8a32d33d0715a4ef0d9a3b
Peashooter101 <Peashooter101@yahoo.com>
2023-08-15 13:49:35 -0700

    Working on Mending Enchant Changes
    Mending Enchantment with this mode should just allow free repairs and not work as mending.
    
    Current issue is that the repair still increases anvil cost.

--- src/main/java/com/adhdmc/MultiMend/listeners/MendItem.java
+++ src/main/java/com/adhdmc/MultiMend/listeners/MendItem.java
@@ -1,87 +1,46 @@
 package com.adhdmc.MultiMend.listeners;
 
-import com.adhdmc.MultiMend.ConfigHandler;
+import net.minecraft.world.inventory.AnvilMenu;
+import org.bukkit.craftbukkit.v1_20_R1.block.impl.CraftAnvil;
+import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftInventoryAnvil;
+import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftItemStack;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.event.player.PlayerItemMendEvent;
-import com.adhdmc.MultiMend.ConfigHandler.Rounding;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.ItemStack;
 
 public class MendItem implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onMendItem(PlayerItemMendEvent e) {
-        int mendLevel = e.getItem().getEnchantmentLevel(Enchantment.MENDING);
-
-        // Randomness Check
-        if (didRandomFail(mendLevel)) {
-            e.setCancelled(true);
-            return;
-        }
-
-        int xp = e.getExperienceOrb().getExperience();
-        e.getPlayer().sendMessage("XP Start: " + xp);
-        int repair = xp / ConfigHandler.getXpCost();
-        e.getPlayer().sendMessage("Repair Start: " + repair);
-        xp %= ConfigHandler.getXpCost();
-        e.getPlayer().sendMessage("XP After: " + xp);
-
-        // Base Multiplier
-        if (ConfigHandler.isMultiplierEnabled()) {
-            double multi = ConfigHandler.getMultiplier();
-            repair = round(multi*repair, ConfigHandler.getMultiplierRounding());
-        }
-
-        // Bonuses
-        if (ConfigHandler.isBonusEnabled()) {
-            int bonus = ConfigHandler.getBonus();
-
-            if (ConfigHandler.isBonusLevelBased()) {
-                if (ConfigHandler.isBonusUsingMultiplier()) {
-                    double multi = ConfigHandler.getBonusMultiplier();
-                    bonus = round(bonus*mendLevel*multi, ConfigHandler.getBonusRounding());
-                } else {
-                    bonus = bonus*mendLevel;
-                }
-            }
-            repair += bonus;
-        }
-
-        // Attempt to resolve StackOverflow error.
-        if (repair == 0) {
-            e.setCancelled(true);
-            return;
-        }
-        e.setRepairAmount(repair);
-        e.getPlayer().sendMessage("Repair Final: " + repair);
-        e.getExperienceOrb().setExperience(xp);
-        e.getPlayer().sendMessage("XP Final: " + xp);
+        e.setCancelled(true);
     }
 
-    /**
-     * Determines if the mending event is cancelled due to randomness check.
-     * @param level Mending Level
-     * @return True if randomness check failed, otherwise False
-     */
-    private boolean didRandomFail(int level) {
-        if (ConfigHandler.isRandomnessEnabled()) {
-            int roll = (int) (Math.random()*100 + 1);
-            if (ConfigHandler.isRandomnessLevelBased()) {
-                roll = roll*level;
-            }
-            return roll >= ConfigHandler.getRandomnessChance();
-        }
-        return false;
+    @EventHandler
+    public void onAnvilInteract(PrepareAnvilEvent event) {
+        AnvilInventory anvil = event.getInventory();
+        if (anvil.getFirstItem() == null || anvil.getSecondItem() == null) return;
+        if (!anvil.getFirstItem().containsEnchantment(Enchantment.MENDING)) return;
+        if (!anvil.getSecondItem().canRepair(anvil.getFirstItem())) return;
+        if (anvil.getResult() == null) return;
+        /* Game reads repair cost as 2x + 1, where x is the number of uses.
+         * Cost of any repair will double the price.
+         */
+//        ItemStack result = anvil.getFirstItem();
+//        net.minecraft.world.item.ItemStack resultHandle = ((CraftItemStack) result).handle;
+//        resultHandle.setRepairCost(anvil.getRepairCost()-1);
+//        anvil.setRepairCost(0);
+//        anvil.setResult(result);
+        anvil.setRepairCost(0);
+        anvil.setMaximumRepairCost();
     }
 
-    private int round(double input, Rounding rounding) {
-        switch (rounding) {
-            case UP -> input = Math.ceil(input);
-            case DOWN -> input = Math.floor(input);
-            default -> input = Math.round(input);
-        }
-        return (int) input;
+    public int calculatePreviousCost(int cost) {
+        return Math.max(((cost - 1) / 2), 0);
     }
 
 }
